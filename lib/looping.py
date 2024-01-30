c3126a28e94703a9028fde40bf9119fc4c48769a
Alan Silva <silzen.ent@gmail.com>
2024-01-29 17:06:54 -0700

    Completed in review lab: Loops

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,39 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    num = 10
+    while(num > 0):
+        print(num)
+        num -= 1
+    print('Happy New Year!')
+    
+    # for i in range(10, 0, -1):
+    #     print(i)
+    # print('Happy New Year!')
+happy_new_year()
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    return [i**2 for i in int_list]
+            # if i > 0 and i**0.5 % 1 == 0] - ignore 
+    # new_list = []
+    # for i in int_list:
+    #     i = i**2
+    #     new_list.append(i)
+    # return new_list
+print(square_integers([1, 2, 3, 4, 5]))
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1, 101):
+        if i%15 == 0:
+            print('FizzBuzz')
+        elif i%5 == 0:
+            print('Buzz')
+        elif i%3 == 0:
+            print('Fizz')
+        else:
+            print(i)
+fizzbuzz()
+
